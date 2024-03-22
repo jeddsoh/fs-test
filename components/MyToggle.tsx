2e4e42a964b87f577cc92ae3c94cafabf7472dca
Jedd Soh <jeddsoh@gmail.com>
2024-03-21 20:12:26 -0500

    wip: add Toggle component to root page

---
+++ components/MyToggle.tsx
@@ -0,0 +1,9 @@
+ import { Toggle } from "@/components/ui/toggle"
+ 
+export function ToggleOutline() {
+  return (
+    <Toggle variant="outline" aria-label="Toggle italic">
+      <h1>Toggled or nah?</h1>
+    </Toggle>
+  )
+}
